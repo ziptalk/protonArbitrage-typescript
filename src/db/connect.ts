bdd57b75027b7fdf13a13cf43affe92d9f0ef5c8
sskys18 <jcs25822@gmail.com>
2024-10-22 17:24:13 +0900

    fix:db models

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -4,11 +4,9 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-const mongoURI = `mongodb://127.0.0.1:27017/proton`;
-
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
